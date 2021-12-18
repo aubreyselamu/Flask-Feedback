06e8668930cbf18a6c6f3fdfd4301222cce18d8e
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-18 13:21:49 -0500

    added user model

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,3 +8,16 @@ def connect_db(app):
     db.init_app(app)
 
 #Models are below
+class User(db.Model):
+    '''User Model'''
+
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(20), primary_key=True, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+
+
