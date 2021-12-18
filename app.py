f8774af73adcf3ee24be4f89e151501b730fddda
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-18 14:18:00 -0500

    added route to register user and add user to database

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
+from forms import RegisterForm, LoginForm
 
 from models import connect_db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flash_feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True 
 app.config['SECRET_KEY'] = 'Yohanna_12'
@@ -16,3 +18,33 @@ connect_db(app)
 @app.route('/')
 def homepage():
     return redirect('/register')
+
+@app.route('/register', methods=["GET","POST"])
+def show_register_form():
+    '''Show user form and process form by adding a new user'''
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+        db.session.add(new_user)
+        db.session.commit()
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def show_login_form():
+    '''Show login form and process the login form'''
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username=form.username.data
+        password=form.password.data
+
+    return render_template('login.html', form=form)
+
